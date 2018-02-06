84f15691fcf2f912f29f88d3158abe4dd1ab1713
celeron1ghz <celeron1ghz@gmail.com>
2018-02-06 06:23:32 +0000

    classnize

--- handler.js
+++ handler.js
@@ -8,39 +8,54 @@ const aws       = require('aws-sdk');
 const ssm       = new aws.SSM();
 const dynamodb  = new aws.DynamoDB();
 
+class TwitterOAuth {
+  static createInstance(event){
+    return vo(function*(){
+      const key    = (yield ssm.getParameter({ Name: '/twitter_oauth/consumer_key',    WithDecryption: true }).promise() ).Parameter.Value;
+      const secret = (yield ssm.getParameter({ Name: '/twitter_oauth/consumer_secret', WithDecryption: true }).promise() ).Parameter.Value;
+      return new TwitterOAuth(event, key, secret);
+    }).catch(err => {
+      console.log("Error on creating oauth object:", err);
+      throw err;
+    })
+  }
 
-function getOAuthObject(event) {
-  return vo(function*(){
-    const host   = 'https://' + event.headers.Host + '/dev/callback'; // + event.requestContext.path;
-    const key    = (yield ssm.getParameter({ Name: '/twitter_oauth/consumer_key',    WithDecryption: true }).promise() ).Parameter.Value;
-    const secret = (yield ssm.getParameter({ Name: '/twitter_oauth/consumer_secret', WithDecryption: true }).promise() ).Parameter.Value;
-    
-    return new OAuth(
+  constructor(event, key, secret) {
+    this.oauth = new OAuth(
       'https://api.twitter.com/oauth/request_token',
       'https://api.twitter.com/oauth/access_token',
       key,
       secret,
       '1.0A',
-      host,
+      'https://' + event.headers.Host + '/dev/callback', // + event.requestContext.path
       'HMAC-SHA1'
     );
-  }).catch(err => {
-    console.log("Error on creating oauth object:", err);
-    throw err;
-  })
+  }
+
+  getOAuthRequestToken() {
+    return new Promise((resolve, reject) => {
+      this.oauth.getOAuthRequestToken((error, oauth_token, oauth_token_secret, results) => {
+        if (error) { reject(error) }
+        else       { resolve({ oauth_token, oauth_token_secret, results })  }
+      });
+    });
+  }
+
+  getOAuthAccessToken(token, secret, verifier) {
+    return new Promise((resolve,reject) => {
+      this.oauth.getOAuthAccessToken(token, secret, verifier, (error, access_token, access_token_secret, results) => {
+        if (error) { reject(error) }
+        else       { resolve({ access_token, access_token_secret, results })  }
+      });
+    })
+  }
 }
 
 module.exports.auth = (event, context, callback) => {
   vo(function*(){
     const uid   = Cookie.parse(event.headers.Cookie || '').sessid || uniqid();
-    const oauth = yield getOAuthObject(event);
-
-    const auth = yield new Promise((resolve, reject) => {
-      oauth.getOAuthRequestToken((error, oauth_token, oauth_token_secret, results) => {
-        if (error) { reject(error) }
-        else       { resolve({ oauth_token, oauth_token_secret, results })  }
-      });
-    });
+    const oauth = yield TwitterOAuth.createInstance(event);
+    const auth  = yield oauth.getOAuthRequestToken();
 
     yield dynamodb.updateItem({
       Key:                       { "uid": {S:uid} }, 
@@ -67,27 +82,17 @@ module.exports.callback = (event, context, callback) => {
   vo(function*(){
     const query  = event.queryStringParameters;
     const sessid = Cookie.parse(event.headers.Cookie || '').sessid;
-    const oauth  = yield getOAuthObject(event);
-
-    const row = yield dynamodb.getItem({ TableName: "twitter_oauth", Key: { "uid": {S:sessid} } }).promise();
+    const oauth  = yield TwitterOAuth.createInstance(event);
+    const row    = yield dynamodb.getItem({ TableName: "twitter_oauth", Key: { "uid": {S:sessid} } }).promise();
     
     if (!row.Item) {
       throw new Error("Record not found for sessid=" + sessid)
     }
     
     const oauth_token_secret = row.Item.session.S;
+    const ret = yield oauth.getOAuthAccessToken(query.oauth_token, oauth_token_secret, query.oauth_verifier);
     
-    const ret = yield new Promise((resolve,reject) => {
-      oauth.getOAuthAccessToken(query.oauth_token, oauth_token_secret, query.oauth_verifier,
-        function(error, access_token, access_token_secret, results){
-          if (error) { reject(error) }
-          else       { resolve({ access_token, access_token_secret, results })  }
-        }
-      );
-    })
-
     console.log(ret);
-
     callback(null, { statusCode: 200, body: "OK" });
 
   }).catch(err => {
